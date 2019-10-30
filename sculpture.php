256508fe3469c63abaa866ec19d7a61b9c4489b3
Marshall Farrier <marshalldfarrier@gmail.com>
2019-10-29 23:03:08 -0700

    Pricing, refactor.

--- sculpture.php
+++ sculpture.php
@@ -21,34 +21,9 @@
                     <h1 class="jumbotron-heading">Sculpture</h1>
                 </div>
             </section>
-
-            <div class="album py-5 bg-light">
-                <div class="container">
-                    <div class="row">
-                        <?php
-                            $jsonFileContents = file_get_contents($cardDataFile);
-                            $cards = json_decode($jsonFileContents, true);
-                            foreach ($cards as $card) {
-                                $imgPath = $paths['media'] . $card['filename'];
-                                $txt = '' .
-                                    '<div class="col-md-4">' .
-                                        '<div class="card mb-4 shadow-sm">' .
-                                            '<img src="' . $imgPath . '" alt="' . $card['title'] . '" width="100%" height"225" class="img-fluid">' .
-                                            '<div class="card-body">' .
-                                                '<h5 class="card-title">' . $card['title'] . '</h5>' .
-                                                '<h6 class="card-subtitle mb-2 text-muted">' . $card['year'] . '</h6>' .
-                                                '<p class-"card-text"><small class="text-muted">' . $card['medium'] . '. ' . $card['dimensions'] . '</small></p>' .
-                                                '<a href="' . $imgPath . '" class="stretched-link" data-toggle="lightbox"></a>                               ' .
-                                            '</div>' .
-                                        '</div>' .
-                                    '</div>';
-                                echo $txt;
-                            }
-                        ?>
-                        
-                    </div>
-                </div>
-            </div>
+<?php
+    include($paths['templates'] . '_items.php');
+?>
         </main>
         <?php include($paths['templates'] . '_footer.html'); ?>
         <?php include($paths['templates'] . '_scripts.html') ?>
